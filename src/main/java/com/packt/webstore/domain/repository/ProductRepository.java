aca4ed09252137ba31815a01e1c96a0e038bba63
unknown <janusz333333@tlen.pl>
2019-01-08 21:35:00 +0100

    Wydobycie zmiennej tablicowej z adresu URL i wykonanie z jej uzyciem operacji wyszukiwania produktów

--- src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.packt.webstore.domain.repository;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import com.packt.webstore.domain.Product;
 
 public interface ProductRepository {
@@ -7,5 +10,5 @@ public interface ProductRepository {
 	List <Product> getAllProducts();  //atrapa repozytorium przechowywuj¹ca obiekty w pamiêci RAM
 	Product getProductById(String productId);
 	List<Product> getProductsByCategory(String category);
-	
+	Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
 }
